7522fe1277b8f4752db32fd33dd811646688058f
David Santana <santanavidda@gmail.com>
2016-10-29 12:04:24 -0400

    added directions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
+require(["esri/map", "dojo/domReady!", "dojo/parser", "esri/urlUtils", "esri/dijit/Directions"], function(Map, Directions) {
 	map = new Map("map", {
 		basemap: "topo",
 		center: [-122.45, 37.75],
---
+++ scripts/directions.js
@@ -0,0 +1,3 @@
+/**
+ * file for handling direction logic
+*/
