34e5e343531c31a915c577b2aca80fb855c24790
David Santana <santanavidda@gmail.com>
2016-10-29 13:07:14 -0400

    add direction panel

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,14 @@
 
 var map;
-require(["esri/map", "dojo/domReady!", "dojo/parser", "esri/urlUtils", "esri/dijit/Directions"], function(Map, Directions) {
+require(["esri/map", "dojo/parser", "esri/urlUtils", "esri/dijit/Directions", "esri/IdentityManager","dojo/domReady!"],
+function(Map, parser, urlUtils, Directions, esriId) {
+	parser.parse();
 	map = new Map("map", {
 		basemap: "topo",
 		center: [-122.45, 37.75],
 		zoom: 13
 	});
+
+	console.log("1234");
+	startDirection(Directions);
 });
--- scripts/directions.js
+++ scripts/directions.js
@@ -1,3 +1,9 @@
 /**
  * file for handling direction logic
 */
+function startDirection(Directions) {
+	var directions = new Directions({
+		map: map
+	},"dir");
+	directions.startup();
+}
