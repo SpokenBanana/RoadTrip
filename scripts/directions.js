506e7d7e2511ead35784883e01a7e018a2ad9228
Milind Lingineni <milind.lingineni@gmail.com>
2016-10-29 15:05:10 -0400

    Stored all streets en route

--- scripts/directions.js
+++ scripts/directions.js
@@ -6,4 +6,25 @@ function startDirection(Directions) {
 		map: map
 	},"dir");
 	directions.startup();
+    directions.on("directions-finish", function(event) {
+        var streetArr = new Set();
+        console.log(event);
+        console.log(event.target.directions.strings);
+        var strings = event.target.directions.strings;
+        streetArr.add(strings[7][1].string);
+        for(var i = 0; i < strings.length; i++)
+        {
+            if(strings[i] != null) 
+            {
+                for(var j = 0; j < strings[i].length; j++)
+                {
+                    streetArr.add(strings[i][j].string);
+                }
+        
+            }
+        }
+        streetArr.forEach(function(value) {
+          console.log(value);
+        });
+    });
 }
