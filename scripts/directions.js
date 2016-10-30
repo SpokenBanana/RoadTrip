e1db401f6cd1e0e80989522e8195457f491df6ac
Milind Lingineni <milind.lingineni@gmail.com>
2016-10-29 20:45:27 -0400

    Fixed input bug

--- scripts/directions.js
+++ scripts/directions.js
@@ -8,22 +8,25 @@ function startDirection(Directions) {
 	},"dir");
 	directions.startup();
     directions.on("directions-finish", function(event) {
-        var streetArr = new Set();
-        var strings = event.target.directions.strings;
-        for(var i = 0; i < strings.length; i++)
+        if(event.target.directions != null) 
         {
-            if(strings[i] != null) 
+            var streetArr = new Set();
+            var strings = event.target.directions.strings;
+            for(var i = 0; i < strings.length; i++)
             {
-                for(var j = 0; j < strings[i].length; j++)
+                if(strings[i] != null) 
                 {
-                    streetArr.add(strings[i][j].string);
+                    for(var j = 0; j < strings[i].length; j++)
+                    {
+                        streetArr.add(strings[i][j].string);
+                    }
+
                 }
-        
             }
+            streetArr.forEach(function(value) {
+              console.log(value);
+            });
         }
-        streetArr.forEach(function(value) {
-          console.log(value);
-        });
         //streetArr is a set that holds all the streets en route
     });
 }
